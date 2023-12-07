aeefcacbc9c48aa5fad658b07f046d20a0b863c7
masiboss <32394683+masiboss@users.noreply.github.com>
2023-12-07 19:34:11 +0100

    fix cpu version not included

--- src/osx/btop_collect.cpp
+++ src/osx/btop_collect.cpp
@@ -229,7 +229,7 @@ namespace Cpu {
 				name.clear();
 		} else if (v_contains(name_vec, "Apple"s)) {
 			auto apple_pos = v_index(name_vec, "Apple"s);
-			name = (apple_pos < name_vec.size() - 1 ? name_vec.at(apple_pos + 1) : "");
+			name = (apple_pos < name_vec.size() - 1 ? name_vec.at(apple_pos + 1) : "") + ' ' + (apple_pos < name_vec.size() - 1 ? name_vec.at(apple_pos + 2) : "");
 		} else
 			name.clear();
 
