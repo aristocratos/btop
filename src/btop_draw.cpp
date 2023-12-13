1ad1418771bd69c70674388d64a4965f9f7d0582
jkre <usokk@student.kit.edu>
2023-12-14 00:58:17 +0100

    remove redundant space

--- src/btop_draw.cpp
+++ src/btop_draw.cpp
@@ -736,7 +736,7 @@ namespace Cpu {
 
 				out += Mv::to(y, bat_pos) + title_left + Theme::c("title") + Fx::b + "BAT" + bat_symbol + ' ' + str_percent
 					+ (Term::width >= 100 ? Fx::ub + ' ' + bat_meter(percent) + Fx::b : "")
-					+ (not str_time.empty() ? ' ' + Theme::c("title") + str_time : " ") + (not str_watts.empty() ? ' ' + Theme::c("title") + Fx::b + str_watts : " ") + Fx::ub + title_right;
+					+ (not str_time.empty() ? ' ' + Theme::c("title") + str_time : "") + (not str_watts.empty() ? ' ' + Theme::c("title") + Fx::b + str_watts : " ") + Fx::ub + title_right;
 			}
 		}
 		else if (bat_pos > 0) {
@@ -2239,3 +2239,4 @@ namespace Draw {
 		}
 	}
 }
+
