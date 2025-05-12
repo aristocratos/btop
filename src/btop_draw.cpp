aristocratos/btop

commit 2538d89ed97bd98b8c092ca73ec75be7db888467

perf: use string view everywhere
This has the potential to avoid allocations when something non allocated
is implicitly converted.

src/btop_draw.cpp
@@ -16,12 +16,13 @@ indent = tab
 tab-size = 4
 */
 
-#include <array>
 #include <algorithm>
+#include <array>
 #include <cmath>
 #include <ranges>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "btop_draw.hpp"
@@ -31,6 +32,7 @@ tab-size = 4
 #include "btop_tools.hpp"
 #include "btop_input.hpp"
 #include "btop_menu.hpp"
+#include <fmt/format.h>
 
 
 using std::array;
@@ -151,7 +153,7 @@ namespace Draw {
 		upos = ulen(this->text);
 	}
 
-	bool TextEdit::command(const string& key) {
+	bool TextEdit::command(const std::string_view key) {
 		if (key == "left" and upos > 0) {
 			upos--;
 			pos = uresize(text, upos).size();
@@ -193,7 +195,7 @@ namespace Draw {
 				upos++;
 			}
 			else {
-				const string first = uresize(text, upos) + key;
+				const auto first = fmt::format("{}{}", uresize(text, upos), key);
 				text = first + text.substr(pos);
 				upos++;
 				pos = first.size();
@@ -245,9 +247,10 @@ namespace Draw {
 		this->text.clear();
 	}
 
-	string createBox(const int x, const int y, const int width,
-					 const int height, string line_color, bool fill,
-					 const string title, const string title2, const int num) {
+	string createBox(
+			const int x, const int y, const int width, const int height, string line_color, bool fill, const std::string_view title,
+			const std::string_view title2, const int num
+	) {
 		string out;
 
 		if (line_color.empty())
@@ -283,12 +286,16 @@ namespace Draw {
 
 		//? Draw titles if defined
 		if (not title.empty()) {
-			out += Mv::to(y, x + 2) + Symbols::title_left + Fx::b + numbering + Theme::c("title") + title
-				+  Fx::ub + line_color + Symbols::title_right;
+			out += fmt::format(
+				"{}{}{}{}{}{}{}{}{}", Mv::to(y, x + 2), Symbols::title_left, Fx::b, numbering, Theme::c("title"), title, Fx::ub,
+				line_color, Symbols::title_right
+			);
 		}
 		if (not title2.empty()) {
-			out += Mv::to(y + height - 1, x + 2) + Symbols::title_left_down + Fx::b + numbering + Theme::c("title") + title2
-				+  Fx::ub + line_color + Symbols::title_right_down;
+			out += fmt::format(
+				"{}{}{}{}{}{}{}{}{}", Mv::to(y, x + 2), Symbols::title_left, Fx::b, numbering, Theme::c("title"), title2, Fx::ub,
+				line_color, Symbols::title_right_down
+			);
 		}
 
 		return out + Fx::reset + Mv::to(y + 1, x + 1);
@@ -1482,7 +1489,7 @@ namespace Proc {
 
 	string box;
 
-	int selection(const string& cmd_key) {
+	int selection(const std::string_view cmd_key) {
 		auto start = Config::getI("proc_start");
 		auto selected = Config::getI("proc_selected");
 		auto last_selected = Config::getI("proc_last_selected");
@@ -1526,7 +1533,7 @@ namespace Proc {
 			if (selected > 0) selected = select_max;
 		}
 		else if (cmd_key.starts_with("mousey")) {
-			int mouse_y = std::stoi(cmd_key.substr(6));
+			int mouse_y = std::atoi(cmd_key.substr(6).data());
 			start = clamp((int)round((double)mouse_y * (numpids - select_max - 2) / (select_max - 2)), 0, max(0, numpids - select_max));
 		}
 

src/btop_draw.hpp
@@ -18,11 +18,12 @@ tab-size = 4
 
 #pragma once
 
-#include <string>
-#include <vector>
 #include <array>
-#include <unordered_map>
 #include <deque>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
 
 using std::array;
 using std::deque;
@@ -72,15 +73,16 @@ namespace Draw {
 		string text;
 		TextEdit();
 		explicit TextEdit(string text, bool numeric=false);
-		bool command(const string& key);
+		bool command(const std::string_view key);
 		string operator()(const size_t limit=0);
 		void clear();
 	};
 
 	//* Create a box and return as a string
-	string createBox(const int x, const int y, const int width,
-		const int height, string line_color = "", bool fill = false,
-		const string title = "", const string title2 = "", const int num = 0);
+	string createBox(
+			const int x, const int y, const int width, const int height, string line_color = "", bool fill = false,
+			const std::string_view title = "", const std::string_view title2 = "", const int num = 0
+	);
 
 	bool update_clock(bool force = false);
 

src/btop_input.cpp
@@ -21,6 +21,7 @@ tab-size = 4
 #include <vector>
 #include <thread>
 #include <mutex>
+#include <fmt/format.h>
 #include <signal.h>
 #include <sys/select.h>
 #include <utility>
@@ -203,7 +204,7 @@ namespace Input {
 		// do not need it, actually
 	}
 
-	void process(const string& key) {
+	void process(const std::string_view key) {
 		if (key.empty()) return;
 		try {
 			auto filtering = Config::getB("proc_filtering");
@@ -213,7 +214,7 @@ namespace Input {
 			//? Global input actions
 			if (not filtering) {
 				bool keep_going = false;
-				if (str_to_lower(key) == "q") {
+				if (key == "q") {
 					clean_quit(0);
 				}
 				else if (is_in(key, "escape", "m")) {
@@ -229,7 +230,7 @@ namespace Input {
 					return;
 				}
 				else if (key.size() == 1 and isint(key)) {
-					auto intKey = stoi(key);
+					auto intKey = std::atoi(key.data());
 				#ifdef GPU_SUPPORT
 					static const array<string, 10> boxes = {"gpu5", "cpu", "mem", "net", "proc", "gpu0", "gpu1", "gpu2", "gpu3", "gpu4"};
 					if ((intKey == 0 and Gpu::count < 5) or (intKey >= 5 and intKey - 4 > Gpu::count))
@@ -540,8 +541,7 @@ namespace Input {
 		}
 
 		catch (const std::exception& e) {
-			throw std::runtime_error("Input::process(\"" + key + "\") : " + string{e.what()});
+			throw std::runtime_error { fmt::format(R"(Input::process("{}"))", e.what()) };
 		}
 	}
-
 }

src/btop_input.hpp
@@ -18,11 +18,12 @@ tab-size = 4
 
 #pragma once
 
-#include <string>
-#include <atomic>
 #include <array>
-#include <unordered_map>
+#include <atomic>
 #include <deque>
+#include <string>
+#include <string_view>
+#include <unordered_map>
 
 using std::array;
 using std::atomic;
@@ -72,6 +73,6 @@ namespace Input {
 	void clear();
 
 	//* Process actions for input <key>
-	void process(const string& key);
+	void process(const std::string_view key);
 
 }

src/btop_menu.cpp
@@ -16,20 +16,22 @@ indent = tab
 tab-size = 4
 */
 
-#include <deque>
-#include <unordered_map>
-#include <array>
-#include <signal.h>
-#include <errno.h>
-#include <cmath>
-#include <filesystem>
-
 #include "btop_menu.hpp"
-#include "btop_tools.hpp"
+
 #include "btop_config.hpp"
-#include "btop_theme.hpp"
 #include "btop_draw.hpp"
 #include "btop_shared.hpp"
+#include "btop_theme.hpp"
+#include "btop_tools.hpp"
+
+#include <errno.h>
+#include <signal.h>
+
+#include <array>
+#include <cmath>
+#include <filesystem>
+#include <unordered_map>
+#include <utility>
 
 using std::array;
 using std::ceil;
@@ -798,7 +800,7 @@ namespace Menu {
 	};
 
 	msgBox::msgBox() {}
-	msgBox::msgBox(int width, int boxtype, const vector<string>& content, string title)
+	msgBox::msgBox(int width, int boxtype, const vector<string>& content, const std::string_view title)
 	: width(width), boxtype(boxtype) {
 		auto tty_mode = Config::getB("tty_mode");
 		auto rounded = Config::getB("rounded_corners");
@@ -1597,7 +1599,7 @@ static int optionsMenu(const string& key) {
 	};
 	bitset<8> menuMask;
 
-	void process(string key) {
+	void process(const std::string_view key) {
 		if (menuMask.none()) {
 			Menu::active = false;
 			Global::overlay.clear();
@@ -1627,7 +1629,7 @@ static int optionsMenu(const string& key) {
 
 		}
 
-		auto retCode = menuFunc.at(currentMenu)(key);
+		auto retCode = menuFunc.at(currentMenu)(key.data());
 		if (retCode == Closed) {
 			menuMask.reset(currentMenu);
 			mouse_mappings.clear();

src/btop_menu.hpp
@@ -18,10 +18,11 @@ tab-size = 4
 
 #pragma once
 
-#include <string>
 #include <atomic>
-#include <vector>
 #include <bitset>
+#include <string>
+#include <string_view>
+#include <vector>
 
 #include "btop_input.hpp"
 
@@ -61,7 +62,7 @@ namespace Menu {
 			Select
 		};
 		msgBox();
-		msgBox(int width, int boxtype, const vector<string>& content, string title);
+		msgBox(int width, int boxtype, const vector<string>& content, std::string_view title);
 
 		//? Draw and return box as a string
 		string operator()();
@@ -87,7 +88,7 @@ namespace Menu {
 	};
 
 	//* Handles redirection of input for menu functions and handles return codes
-	void process(string key="");
+	void process(const std::string_view key = "");
 
 	//* Show a menu from enum Menu::Menus
 	void show(int menu, int signal=-1);

src/btop_shared.hpp
@@ -23,9 +23,11 @@ tab-size = 4
 #include <deque>
 #include <filesystem>
 #include <string>
+#include <string_view>
 #include <tuple>
-#include <vector>
 #include <unordered_map>
+#include <vector>
+
 #include <unistd.h>
 
 // From `man 3 getifaddrs`: <net/if.h> must be included before <ifaddrs.h>
@@ -67,7 +69,6 @@ namespace Global {
 }
 
 namespace Runner {
-
 	extern atomic<bool> active;
 	extern atomic<bool> reading;
 	extern atomic<bool> stopping;
@@ -414,7 +415,7 @@ namespace Proc {
 	auto collect(bool no_update = false) -> vector<proc_info>&;
 
 	//* Update current selection and view, returns -1 if no change otherwise the current selection
-	int selection(const string& cmd_key);
+	int selection(const std::string_view cmd_key);
 
 	//* Draw contents of proc box using <plist> as data source
 	string draw(const vector<proc_info>& plist, bool force_redraw = false, bool data_same = false);

src/btop_tools.cpp
@@ -26,6 +26,7 @@ tab-size = 4
 #include <optional>
 #include <ranges>
 #include <sstream>
+#include <string_view>
 #include <utility>
 
 #include <fcntl.h>
@@ -33,7 +34,6 @@ tab-size = 4
 #include <termios.h>
 #include <unistd.h>
 
-#include "unordered_map"
 #include "widechar_width.hpp"
 #include "btop_shared.hpp"
 #include "btop_tools.hpp"
@@ -211,11 +211,11 @@ namespace Term {
 
 namespace Tools {
 
-	size_t wide_ulen(const string& str) {
+	size_t wide_ulen(const std::string_view str) {
 		unsigned int chars = 0;
 		try {
 			std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-			auto w_str = conv.from_bytes((str.size() > 10000 ? str.substr(0, 10000).c_str() : str.c_str()));
+			auto w_str = conv.from_bytes((str.size() > 10000 ? str.substr(0, 10000).data() : str.data()));
 
 			for (auto c : w_str) {
 				chars += utf8::wcwidth(c);
@@ -228,7 +228,7 @@ namespace Tools {
 		return chars;
 	}
 
-	size_t wide_ulen(const std::wstring& w_str) {
+	size_t wide_ulen(const std::wstring_view w_str) {
 		unsigned int chars = 0;
 
 		for (auto c : w_str) {
@@ -682,7 +682,7 @@ namespace Logger {
 		loglevel = v_index(log_levels, level);
 	}
 
-	void log_write(const Level level, const string& msg) {
+	void log_write(const Level level, const std::string_view msg) {
 		if (loglevel < level or !logfile.has_value()) {
 			return;
 		}

commit d3f12f124c9099ea5620027626c156db0e906233

Fix incorrect positioning and start symbol of second title introduced in 2538d89ed97bd98b8c092ca73ec75be7db888467

src/btop_draw.cpp
@@ -293,7 +293,7 @@ namespace Draw {
 		}
 		if (not title2.empty()) {
 			out += fmt::format(
-				"{}{}{}{}{}{}{}{}{}", Mv::to(y, x + 2), Symbols::title_left, Fx::b, numbering, Theme::c("title"), title2, Fx::ub,
+				"{}{}{}{}{}{}{}{}{}", Mv::to(y + height - 1, x + 2), Symbols::title_left_down, Fx::b, numbering, Theme::c("title"), title2, Fx::ub,
 				line_color, Symbols::title_right_down
 			);
 		}