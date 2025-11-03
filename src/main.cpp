5b32da5a3a950ac866fb591ddc0f97a8b625d717
Alvaro Parker <64918109+AlvaroParker@users.noreply.github.com>
2025-11-03 10:27:11 -0300

    fix: skip first argument of argv (#1339)
    Introduced in https://github.com/aristocratos/btop/pull/1325

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,5 @@
 #include <vector>
 
 auto main(int argc, const char* argv[]) -> int {
-	return btop_main(std::views::counted(argv, argc) | std::ranges::to<std::vector<std::string_view>>());
+	return btop_main(std::views::counted(std::next(argv), argc - 1) | std::ranges::to<std::vector<std::string_view>>());
 }
