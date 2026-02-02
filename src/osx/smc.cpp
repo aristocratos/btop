86e06e40050f1c16fea0d296ab5ae9dd60ab9911
Steffen Winter <steffen.winter@proton.me>
2026-02-02 11:12:41 +0100

    style: apply clang-format
    Closes: https://github.com/aristocratos/btop/issues/573

--- src/osx/smc.cpp
+++ src/osx/smc.cpp
@@ -4,7 +4,7 @@
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+		   http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,9 +19,9 @@ tab-size = 4
 #include "smc.hpp"
 
 static constexpr size_t MaxIndexCount = sizeof("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ") - 1;
-static constexpr const char *KeyIndexes = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static constexpr const char* KeyIndexes = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-static UInt32 _strtoul(char *str, int size, int base) {
+static UInt32 _strtoul(char* str, int size, int base) {
 	UInt32 total = 0;
 	int i;
 
@@ -35,13 +35,11 @@ static UInt32 _strtoul(char *str, int size, int base) {
 	return total;
 }
 
-static void _ultostr(char *str, UInt32 val) {
+static void _ultostr(char* str, UInt32 val) {
 	str[0] = '\0';
-	snprintf(str, 5, "%c%c%c%c",
-			(unsigned int)val >> 24,
-			(unsigned int)val >> 16,
-			(unsigned int)val >> 8,
-			(unsigned int)val);
+	snprintf(
+		str, 5, "%c%c%c%c", (unsigned int)val >> 24, (unsigned int)val >> 16, (unsigned int)val >> 8, (unsigned int)val
+	);
 }
 
 namespace Cpu {
@@ -69,7 +67,7 @@ namespace Cpu {
 		IOServiceClose(conn);
 	}
 
-	long long SMCConnection::getSMCTemp(char *key) {
+	long long SMCConnection::getSMCTemp(char* key) {
 		SMCVal_t val;
 		kern_return_t result;
 		result = SMCReadKey(key, &val);
@@ -87,9 +85,10 @@ namespace Cpu {
 
 	// core means physical core in SMC, while in core map it's cpu threads :-/ Only an issue on hackintosh?
 	// this means we can only get the T per physical core
-	// another issue with the SMC API is that the key is always 4 chars -> what with systems with more than 9 physical cores?
-	// no Mac models with more than 18 threads are released, so no problem so far
-	// according to VirtualSMC docs (hackintosh fake SMC) the enumeration follows with alphabetic chars - not implemented yet here (nor in VirtualSMC)
+	// another issue with the SMC API is that the key is always 4 chars -> what with systems with more than 9 physical
+	// cores? no Mac models with more than 18 threads are released, so no problem so far according to VirtualSMC docs
+	// (hackintosh fake SMC) the enumeration follows with alphabetic chars - not implemented yet here (nor in
+	// VirtualSMC)
 	long long SMCConnection::getTemp(int core) {
 		char key[] = SMC_KEY_CPU_TEMP;
 		if (core >= 0) {
@@ -107,7 +106,7 @@ namespace Cpu {
 		return result;
 	}
 
-	kern_return_t SMCConnection::SMCReadKey(UInt32Char_t key, SMCVal_t *val) {
+	kern_return_t SMCConnection::SMCReadKey(UInt32Char_t key, SMCVal_t* val) {
 		kern_return_t result;
 		SMCKeyData_t inputStructure;
 		SMCKeyData_t outputStructure;
@@ -120,8 +119,7 @@ namespace Cpu {
 		inputStructure.data8 = SMC_CMD_READ_KEYINFO;
 
 		result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
-		if (result != kIOReturnSuccess)
-			return result;
+		if (result != kIOReturnSuccess) return result;
 
 		val->dataSize = outputStructure.keyInfo.dataSize;
 		_ultostr(val->dataType, outputStructure.keyInfo.dataType);
@@ -129,26 +127,30 @@ namespace Cpu {
 		inputStructure.data8 = SMC_CMD_READ_BYTES;
 
 		result = SMCCall(KERNEL_INDEX_SMC, &inputStructure, &outputStructure);
-		if (result != kIOReturnSuccess)
-			return result;
+		if (result != kIOReturnSuccess) return result;
 
 		memcpy(val->bytes, outputStructure.bytes, sizeof(outputStructure.bytes));
 
 		return kIOReturnSuccess;
 	}
 
-	kern_return_t SMCConnection::SMCCall(int index, SMCKeyData_t *inputStructure, SMCKeyData_t *outputStructure) {
+	kern_return_t SMCConnection::SMCCall(int index, SMCKeyData_t* inputStructure, SMCKeyData_t* outputStructure) {
 		size_t structureInputSize;
 		size_t structureOutputSize;
 
 		structureInputSize = sizeof(SMCKeyData_t);
 		structureOutputSize = sizeof(SMCKeyData_t);
 
-		return IOConnectCallStructMethod(conn, index,
-										 // inputStructure
-										 inputStructure, structureInputSize,
-										 // outputStructure
-										 outputStructure, &structureOutputSize);
+		return IOConnectCallStructMethod(
+			conn,
+			index,
+			// inputStructure
+			inputStructure,
+			structureInputSize,
+			// outputStructure
+			outputStructure,
+			&structureOutputSize
+		);
 	}
 
-}  // namespace Cpu
+} // namespace Cpu
